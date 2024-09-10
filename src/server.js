5a02c2006ee43211889186a2b6205e5ef8c843f7
AnastasiiaMac <for_nastia@ukr.net>
2024-09-10 10:35:42 +0200

    routers added

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
-import * as contactServices from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
 export const setupServer = () => {
   const app = express();
   const logger = pino({
@@ -14,30 +14,7 @@ export const setupServer = () => {
   app.use(cors());
   app.use(express.json());
 
-  app.get('/contacts', async (req, res) => {
-    const data = await contactServices.getAllContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data,
-    });
-  });
-
-  app.get('/contacts/:id', async (req, res) => {
-    console.log(req.params);
-    const { id } = req.params;
-    const data = await contactServices.getContactById(id);
-    if (!data) {
-      return res.status(404).json({
-        message: 'Contact not found',
-      });
-    }
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${id}!`,
-      data,
-    });
-  });
+  app.use('/contacts', contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({
