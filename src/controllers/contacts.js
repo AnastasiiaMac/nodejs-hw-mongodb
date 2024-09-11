f95f2ccb0f313308b746efb58ce1d1c498d57718
AnastasiiaMac <for_nastia@ukr.net>
2024-09-11 19:48:13 +0200

    routers  added

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -24,3 +24,36 @@ export const getContactByIdController = async (req, res) => {
     data,
   });
 };
+
+export const addContactController = async (req, res) => {
+  const data = await contactServices.createContact(req.body);
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data,
+  });
+};
+
+export const patchContactController = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactServices.updateContact({ _id: id }, req.body);
+
+  if (!result) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.data,
+  });
+};
+
+export const deleteContactController = async (req, res) => {
+  const { id } = req.params;
+  const data = await contactServices.deleteContact({ _id: id });
+  if (!data) {
+    throw createHttpError(404, 'Contact not found');
+  }
+  res.status(204).send();
+};
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,15 @@
 const errorHandler = (error, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
+  const { status = 500, message } = error;
+  res.status(status).json({
+    message,
   });
 };
+
 export default errorHandler;
+
+// const errorHandler = (error, req, res, next) => {
+//   res.status(500).json({
+//     message: 'Something went wrong',
+//   });
+// };
+// export default errorHandler;
