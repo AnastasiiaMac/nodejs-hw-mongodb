2ecfa48cd04f368af686ca5d7ddda9c3295cc1f8
AnastasiiaMac <for_nastia@ukr.net>
2024-09-30 12:43:28 +0200

    reset password added

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,5 @@
 import * as authServices from '../services/auth.js';
+
 const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
@@ -54,4 +55,22 @@ export const logoutController = async (req, res) => {
   res.clearCookie('sessionId');
   res.clearCookie('refreshToken');
   res.status(204).send();
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await authServices.requestResetToken(req.body.email);
+  res.json({
+    message: 'Reset password email has been successfully sent.',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await authServices.resetPassword(req.body);
+  res.json({
+    message: 'Password has been successfully reset!',
+    status: 200,
+    data: {},
+  });
 };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,7 +2,12 @@ import { Router } from 'express';
 import * as authControllers from '../controllers/auth.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../utils/validateBody.js';
-import { userRegisterSchema, userLoginSchema } from '../validation/users.js';
+import {
+  userRegisterSchema,
+  userLoginSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/users.js';
 
 const authRouter = Router();
 authRouter.post(
@@ -18,5 +23,15 @@ authRouter.post(
 );
 authRouter.post('/refresh', ctrlWrapper(authControllers.refreshController));
 authRouter.post('/logout', ctrlWrapper(authControllers.logoutController));
+authRouter.post(
+  '/send-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(authControllers.requestResetEmailController),
+);
 
+authRouter.post(
+  '/reset-pwd',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(authControllers.resetPasswordController),
+);
 export default authRouter;
